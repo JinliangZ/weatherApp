351d9428ce79b7ac6d87dbb96b834a88508dbfce
Jinliang <33104348+JinliangZ@users.noreply.github.com>
2021-08-24 00:04:28 -0400

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 class Forecast{
     constructor(){
-        this.key = 'mFqw9jieVs1dPQxZvDftoFDRUbl4AGPU';
+        this.key = 'your_key';
         this.cityURI =  'https://dataservice.accuweather.com/locations/v1/cities/search';
         this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
     }
